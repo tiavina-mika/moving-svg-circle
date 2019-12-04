855a36a3a1e9761d95e945a0b2926ffbfed9f4b9
TiavinaMichael <ralainirinatiavina@gmail.com>
2019-12-04 14:49:51 +0100

    change animation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, lighten } from "@material-ui/core/styles";
 import Circle3 from './circle-ext-1';
 const useStyles = makeStyles({
   container: {
-    backgroundColor: '#191D1F'
+    backgroundColor: '#000'
   },
   circle1: {
-    fill: '#111518',
+    fill: '#0B2926',
     stroke: '#40A9F3',
     strokeWidth: '0.1px',
   },
   circle2: {
-    fill: 'none',
+    fill: '#2CAAA1',
     stroke: '#40A9F3',
     strokeWidth: '0.1px'
   },
@@ -22,33 +22,56 @@ const useStyles = makeStyles({
     strokeWidth: '0.1px'
   },
   circle3: {
-    fill: 'none',
+    fill: 'red',
     stroke: '#40A9F3',
     strokeWidth: '0.1px'
-  }
+  },
+  text: {
+    fill: '#fff',
+    fontSize: 8
+  },
+  circleOut1: {
+    stroke: '#31BCB1',
+    fill: 'none',
+    strokeWidth: '0.4'
+  },
+  circleOut2: {
+    stroke: '#16534E',
+    fill: 'none',
+    strokeWidth: '0.3'
+  },
+
 })
 
 const App = () => {
   const classes = useStyles();
   return (
     <div>
-      <svg viewBox="0 0 250 250" className={classes.container}>
+      <svg viewBox="0 0 300 300" className={classes.container}>
           <g>
-            <circle r="60" cx="120" cy="120" className={classes.shadow}/>
-            <path d ="M60,120a60,60 0 1,0 120,0a60,60 0 1,0 -120,0"
-            className={classes.circle1}/>
-            <text x="90" y="120" fill="red">Some Text</text>
+            {/* <circle r="60" cx="120" cy="120" className={classes.shadow}/>
+            <circle r="60" cx="120" cy="120" className={classes.shadow}/> */}
+            {/* <path d ="M60,120a60,60 0 1,0 120,0a60,60 0 1,0 -120,0"
+            className={classes.circle1}/> */}
 
-            <circle r="50" cx="120" cy="120" className={classes.circle2}/>
+            <circle r="100" cx="150" cy="150" className={classes.circle1}/>
+            <circle r="80" cx="150" cy="150" className={classes.circle2}/>
+            <text x="110" y="100" className={classes.text}>Development -</text>
           </g>
-          <circle className={classes.circle3}>
-            <animateMotion dur="20s" repeatCount="indefinite"
-            path="M60,120a60,60 0 1,0 120,0a60,60 0 1,0 -120,0" 
-            />
-            <animate attributeName="r" fill="freeze" from="20" to="30" dur="10s"/>
-
-
-          </circle>
+          <g>
+            <circle cx="70" className={classes.circleOut2}>
+              <animate attributeName="r" fill="freeze" from="40" to="45" dur="2s"repeatCount="indefinite"/>
+              <animate attributeName="cy" fill="freeze" from="180" to="190" dur="2s"repeatCount="indefinite"/>
+            
+            </circle>
+            <circle cx="70" className={classes.circleOut1}>
+                {/* <animateMotion dur="20s" repeatCount="indefinite"
+                path="M60,120a60,60 0 1,0 120,0a60,60 0 1,0 -120,0" 
+                /> */}
+                <animate attributeName="r" fill="freeze" from="30" to="35" dur="2s"repeatCount="indefinite"/>
+                <animate attributeName="cy" fill="freeze" from="180" to="190" dur="2s"repeatCount="indefinite"/>
+              </circle>
+          </g>
 
       </svg>
     </div>
